251c856fade97512d73482f9dc8e71321604fa78
katako01 <81860561+katako01@users.noreply.github.com>
2021-04-03 15:50:49 +0100

    Add files via upload

---
+++ katako-91.py
@@ -0,0 +1,30 @@
+print('Question ONE')
+NAME='MAARUF ABDULLAHI KATAKO'
+DEPT='COMPUTER SCIENCE DEPARTMENT'
+print('my name is',NAME,'i am from',DEPT)
+print('Question TWO')
+scare='Boo!'
+
+print(scare)
+print('Question THREE')
+NumbA=7
+NumbB=9
+NumbC=NumbA + NumbB
+print(NumbA,NumbB,NumbC)
+print('Question FOUR')
+clear=12/0.5
+print(clear)
+print('Question FIVE')
+Adding=5.1 + 6
+print(Adding)
+print('Question SIX')
+sure=8%3
+print(sure)
+print('Question SEVEN')
+Result1=2 * 4
+
+Result2=4 + 2
+
+Result3=Result1 * Result2
+
+print(Result3)
